e69d4cac6f725d7c6c9b26417ad34fbfccff5bd8
Yusif <am.yusif530@gmail.com>
2023-02-13 11:19:28 -0800

    done for now

--- src/Components/Custom/Render.js
+++ src/Components/Custom/Render.js
@@ -8,8 +8,7 @@ const Render = () => {
   const [Rend, setRend] = useState([]);
 
   const searchSubmit = async (entry) => {
-    const callBack =
-      await axios.get(`https://pixabay.com/api/?key=29277356-18ae8059fd03787c3002d30ac&q=${entry}&image_type=photo
+    const callBack = await axios.get(`https://pixabay.com/api/?key=29277356-18ae8059fd03787c3002d30ac&q=${entry}&image_type=photo
         `);
     setRend(callBack.data.hits);
   };
--- src/Components/Custom/Reveal.js
+++ src/Components/Custom/Reveal.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const Reveal = ({ images }) => {
   const imageList = images.map((items) => {
-    return <img key={items.id} src={items.webformatURL} alt="" />;
+    return <img key={items.id} src={items.webformatURL} alt="" width="350" height="300"/>;
   });
 
-  return <div>{imageList}</div>;
+  return <div className="card">
+  <div className="card-container">
+    {imageList}
+  </div>
+  </div>;
 };
 
 export default Reveal;
